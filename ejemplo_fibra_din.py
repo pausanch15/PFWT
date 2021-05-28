a993b8ffc7ae0cd343c08384969647d0fb8b21b2
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-05-28 18:29:40 -0300

    Update ejemplo_fibra_din.py

--- ejemplo_fibra_din.py
+++ ejemplo_fibra_din.py
@@ -8,10 +8,11 @@ from skimage.util import random_noise
 from skimage.filters import gaussian
 from ipywidgets import interact
 import Genera_Fibras as gf
+#%%
 
-np.random.seed(3)
-n = 50
-imagenes = gf.crear_im_fibra(n+1,fondo=0.05)
+np.random.seed(12)
+n = 1
+imagenes = gf.crear_im_fibra(n+1,fondo=0.05,salto=10)
 def print_im(frame):
     global imagenes
     plt.figure()
@@ -20,9 +21,19 @@ def print_im(frame):
     plt.colorbar()
     plt.show()
     
-interact(print_im,frame=(0,10,1))
+#interact(print_im,frame=(0,10,1))
 plt.figure(figsize=(7,7))
 plt.imshow(imagenes[-1])
 plt.set_cmap('gray')
 plt.colorbar()
 plt.show()
+
+#%%
+import imageio
+
+np.random.seed(12)
+n = 40
+imagenes = gf.crear_im_fibra(n+1,fondo=0.05,salto=5)
+
+imageio.mimsave('C:\\Users\\tomfe\\Documents\\TOMAS\\Facultad\\Laboratorio 6\\fibra.gif',imagenes,fps=12)
+#%%
