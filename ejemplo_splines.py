bcee2ca1c0dd129fafb83835dd91b0c047a427e5
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-05-30 18:28:17 -0300

    Update ejemplo_splines.py

--- ejemplo_splines.py
+++ ejemplo_splines.py
@@ -10,6 +10,7 @@ from skimage.morphology import thin, skeletonize
 from scipy.interpolate import CubicSpline, splev, splrep, splprep
 from scipy.signal import convolve2d, savgol_filter
 from itertools import permutations
+import imageio
 #%%
 image = Image.open('imagenprueba.png') #cargo la imagen
 ima = np.asarray(image) #la convierto en numpy array
@@ -53,7 +54,8 @@ np.random.seed(12)
 n = 40
 imagenes = gf.crear_im_fibra(n+1,fondo=0.05,salto=5)
 #%%
-imt = imagenes[5] < 108
+ff = 21 #problemas: 16,21,24?
+imt = imagenes[ff] < 115
 iml = label(imt)
 prop_reg = regionprops(iml)
 for i in range(len(prop_reg)):
@@ -61,39 +63,40 @@ for i in range(len(prop_reg)):
         fibra = iml==i+1
 fibra = skeletonize(fibra)
 
+#plt.figure()
+plt.set_cmap('gray')
+plt.imshow(imagenes[ff]) #,extent=[500,1000,0,350])
+plt.show()
 plt.figure()
-plt.set_cmap('jet')
+#plt.xlim(left=500,rigt=1000)
 #plt.imshow(imt)
 plt.imshow(fibra)
 plt.show()
 
 plt.figure()
-tramos,bordes = spl.cortar_fibra(fibra)
-
+tramos,bordes = spl.cortar_fibra2(fibra)
+#
 for tr in range(len(tramos)):
     x,y = tramos[tr][:,0],tramos[tr][:,1]
     plt.plot(x,y,'o',color='grey')
-
+#
 tramos = spl.ordenar_fibra(tramos)
 t,curv,xf,yf = spl.pegar_fibra(tramos,bordes)
 
-plt.plot(xf,yf,'r-')
-
+plt.plot(xf,yf,'r-',alpha=0.75)
+#plt.xlim(500,1000)
+#plt.ylim(0,350)
 plt.gca().invert_yaxis()
 plt.grid(True)
-#plt.tick_params(left = False, right = False , labelleft = False ,
-#                labelbottom = False, bottom = False)
+##plt.tick_params(left = False, right = False , labelleft = False ,
+##                labelbottom = False, bottom = False)
 plt.show()
 #%%
 
-np.random.seed(12)
-n = 1
-imagenes = gf.crear_im_fibra(n+1,fondo=0.05,salto=5)
-
 lista_im = []
 for frames in range(len(imagenes)):
-
-    imt = imagenes[0] < 115
+    print(frames)
+    imt = imagenes[frames] < 115
     iml = label(imt)
     prop_reg = regionprops(iml)
     for i in range(len(prop_reg)):
@@ -101,22 +104,24 @@ for frames in range(len(imagenes)):
             fibra = iml==i+1
     fibra = skeletonize(fibra)
 
-    plt.figure()
-    tramos,bordes = spl.cortar_fibra(fibra)
+    tramos,bordes = spl.cortar_fibra2(fibra)
     
-    for tr in range(len(tramos)):
-        x,y = tramos[tr][:,0],tramos[tr][:,1]
-        plt.plot(x,y,'o',color='grey')
+#    for tr in range(len(tramos)):
+#        x,y = tramos[tr][:,0],tramos[tr][:,1]
+#        plt.plot(x,y,'o',color='grey')
     
     tramos = spl.ordenar_fibra(tramos)
     t,curv,xf,yf = spl.pegar_fibra(tramos,bordes)
     
+    plt.imshow(imagenes[frames])
     plt.plot(xf,yf,'r-')
-    
+    plt.xlim(500,1000)
+    plt.ylim(0,350)
     plt.gca().invert_yaxis()
-    plt.grid(True)
-    plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
-    plt.savefig('imageni.png',layout='tight')
+#    plt.grid(True)
+#    plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
+    plt.savefig('imageni.png',bbox_inches='tight')
     lista_im.append(imageio.imread('imageni.png'))
-    
+#%%
+imageio.mimsave('C:\\Users\\tomfe\\Documents\\TOMAS\\Facultad\\Laboratorio 6\\fibrayspline.gif',lista_im,fps=12)
 
