736abf44b087f2a8698778dabb4a297ad69af8be
pausanch15 <paulijsanchez@live.com.ar>
2021-06-25 15:48:56 -0300

    Algunas cuentas mas

--- prueba_estadistica.py
+++ prueba_estadistica.py
@@ -15,12 +15,12 @@ import imageio
 
 #Generamos n fibras
 np.random.seed(12)
-n = 20
+n = 10
 imagenes, fib = gf.crear_im_fibra(n+1, ruido=0.0015, fondo=0.05, salto=50, drift=0)
 fibras = spl.encuentra_fibra(imagenes)
 
 #Probamos solo con una
-ff = 19
+ff = 5
 
 plt.figure()
 plt.set_cmap('gray')
@@ -32,8 +32,63 @@ plt.show()
 plt.figure()
 plt.hist(imagenes[ff].flatten(), bins=100, facecolor='k')
 plt.vlines(np.mean(imagenes[ff].flatten()), 0, 570000, color='r', label='Media')
-
 plt.vlines(np.mean(imagenes[ff].flatten())-3*(np.std(imagenes[ff].flatten())), 0, 570000, color='g', label='Media menos Tres Desvíos')
+plt.legend()
 plt.show()
 
 #Ya cambie la funcion en Func_Splines
+
+#Ahora trato de hacer estadistica.
+fibra = fibras[ff]
+
+tramos, bordes = spl.cortar_fibra(fibra, cortar_ruido=True)
+tramos = spl.ordenar_fibra(tramos)
+curv, spline = spl.pegar_fibra(tramos, bordes, window=21, s=10)
+
+t_spl = np.linspace(0, 1, 10000)
+
+#xf y yf son as coordenadas de la fibra
+xf, yf = splev(t_spl, spline)
+
+#Vemos la fibra que recuperamos
+plt.figure()
+plt.imshow(fibra)    
+plt.plot(xf, yf, 'r-')
+plt.show()
+
+#xo y yo son las coordenadas de la fibra original
+yo, xo = splev(t_spl, fib[ff])
+
+#Comparo la fibra original con la que analizamos
+plt.figure()
+plt.plot(xo, yo, 'g-', label='Fibra Original') 
+plt.plot(xf, yf, 'r-', label='Fibra Obtenida')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot(xf-xo, label='Diferencia en x')
+plt.plot(yf-yo, label='Diferencia en y')
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot(np.abs(xf-xo), label='Diferencia en x')
+plt.plot(np.abs(yf-yo), label='Diferencia en y')
+plt.grid()
+plt.legend()
+plt.show()
+
+#Hago el histograma para la diferencia en x y en y. Cuantos puntos difieren en cada valor?
+plt.figure()
+plt.hist(xf-xo, bins=30, facecolor='blue', label='x', alpha=0.5, edgecolor='k')
+plt.hist(yf-yo, bins=30, facecolor='red', label='y', alpha=0.5, edgecolor='k')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.hist(np.abs(xf-xo), bins=30, facecolor='blue', label='x', alpha=0.5, edgecolor='k')
+plt.hist(np.abs(yf-yo), bins=30, facecolor='red', label='y', alpha=0.5, edgecolor='k')
+plt.legend()
+plt.show()
