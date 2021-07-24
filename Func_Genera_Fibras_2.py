3dc412a30d16ecea912ff9fc0c35fdbd0f37ed21
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-07-24 19:25:00 -0300

    Update Func_Genera_Fibras_2.py

--- Func_Genera_Fibras_2.py
+++ Func_Genera_Fibras_2.py
@@ -5,6 +5,7 @@ from mpl_toolkits import mplot3d
 import matplotlib.animation as animation
 from scipy.interpolate import splev, splrep, splprep
 from skimage.morphology import thin, skeletonize, remove_small_objects, binary_dilation, dilation
+from skimage.draw import line
 from skimage.util import random_noise
 from skimage.filters import gaussian
 
@@ -108,8 +109,47 @@ def genera_im_fibra(fibra, bins=500, sigma=1, ruido=0.003, fondo=0.05):
     
     return im_fibra
 
-def genera_im_dinamica(frames=10, n_fibras=2, bins=400, sigma=1, 
-                       ruido=0.003, fondo=0.05, drift=0, alpha=0.1, Nt=8, N=100):
+def fibra_a_skeleton(xfibra, L=300, S=(1024, 1024), graph=False):
+    """
+    usamos Bresenham’s line algorithm para la conversion,
+    previo escalado de la fibra original
+    """
+    x, y = np.real(xfibra), np.imag(xfibra)
+    # hacemos que sea positiva toda la senal
+    x -= np.min(x)
+    y -= np.min(y)
+    # la escalamos
+    x *= L
+    y *= L
+    # la posicionamos cerca del centro
+    x += S[1]//2
+    y += S[0]//2
+    # imagen fondo blanco (high) fibra negra (low)
+    im = np.ones(S, dtype=np.uint8)
+    for i in range(len(x)-1):
+        rr, cc = line(int(x[i]), int(y[i]), int(x[i+1]), int(y[i+1]))
+        im[rr, cc] = 0
+    if graph == True:
+        plt.figure()
+        plt.imshow(im, cmap='gray')
+        plt.plot(y, x, 'r-o', linewidth=0.75)
+        plt.xlim(0, S[1])
+        plt.ylim(0, S[0])
+        plt.colorbar()
+    return im, x,y 
+
+def curva(x,y,s=0):
+    porder = 3
+    spl, u = splprep([xsg,ysg],s=s)
+    t_spl = np.linspace(0,1,100000)
+#    spline = splev(t_spl,spl)
+    spline_1d = splev(t_spl,spl,der=1)
+    spline_2d = splev(t_spl,spl,der=2)
+    curv = np.abs(spline_2d[0] * spline_1d[1] - spline_1d[0] * spline_2d[1]) / ((spline_1d[0])**2 + (spline_1d[1])**2)**(3/2)
+    return np.max(curv) # t_spl, spline
+
+def genera_im_dinamica(frames=1, n_fibras=2, L=300, sigma=1, 
+                       ruido=0.003, fondo=0.05, alpha=0.1, Nt=8, N=100):
     """
     Genera las imagenes de la dinamica de fibras. Esta función ya genera las fibras.
     n_fibras = la cantidad de fibras que se usa para hacer la dinamica, tiene que ser mayor o igual a 2
@@ -118,37 +158,23 @@ def genera_im_dinamica(frames=10, n_fibras=2, bins=400, sigma=1,
     fibras = []
     for i in range(n_fibras):
         fib = generar_fibra(N=N, L=1, alpha=alpha, Nt=Nt)
+#        x,y = np.real(fib), np.imag(fib)
+#        curm = curva(x,y)
+#        
         fibras.append(fib)
-    
+        
     dinam = generar_dinamica(fibras,frames)
-    
-    
+   
     imagenes = []
     splines = []
-    origen = np.random.random(2) * (1000-bins-200-50) + 250
-    ox, oy = int(origen[0]), int(origen[1])
     for i in range( (n_fibras-1)*frames ):
-        x_f, y_f = np.real(dinam[:,i]), np.imag(dinam[:,i])
-
-        dr = drift*np.random.random(2) - drift/2
-        ox += int(dr[0])
-        oy += int(dr[1])
-        x_f, y_f = x_f*bins + ox, y_f*bins + oy
-        if np.min(x_f) < 0 or np.max(x_f) > 1000:
-            x_f = x_f - ox
-            ox -= 2*int(dr[0])
-            x_f = x_f + 2*ox
-        if np.min(y_f) < 0 or np.max(y_f) > 1000:
-            y_f = y_f - oy
-            oy -= 2*int(dr[1])
-            y_f = y_f + 2*oy
+        
+        ima,x_f,y_f = fibra_a_skeleton(dinam[:,i], L=L)
             
         spl, u = splprep([x_f, y_f], s=0)
-        t_spl = np.linspace(0, 1, len(x_f)*200)
-        fr = splev(t_spl, spl)
         splines.append(spl)
         
-        im_fibra, x_ed, y_ed = np.histogram2d(*fr, 1000, [[0,1000],[0,1000]])
+#        im_fibra, x_ed, y_ed = np.histogram2d(*fr, 1000, [[0,1000],[0,1000]])
     
 #        dr = drift*np.random.random(2) - drift/2
 #        ox += int(dr[0])
@@ -158,18 +184,16 @@ def genera_im_dinamica(frames=10, n_fibras=2, bins=400, sigma=1,
 #        im_fibra = np.zeros((1000, 1000)) #Imagen más grande para poner la fibra
 #        im_fibra[ox:(ox+bins), oy:(oy+bins)] = im_fib
     
-        im_fibra = im_fibra==0
-        im_fibra = dilation(1-im_fibra)
-        im_fibra = np.array(1-im_fibra,dtype='float')
+        im_fibra = ima==1
+#        im_fibra = dilation(1-im_fibra)
+#        im_fibra = np.array(1-im_fibra,dtype='float')
         im_fibra = random_noise(im_fibra, mode='s&p', amount=ruido)
-        ff = np.linspace(0,999,1000)
+        ff = np.linspace(0,999,1024)
         fx,fy = np.meshgrid(ff,ff)
-        im_fibra = (im_fibra + fx*fy / 1000**2 * fondo) / 2
+        im_fibra = (im_fibra + fx*fy / 1024**2 * fondo) / 2
         im_fibra = gaussian(im_fibra, sigma=sigma)
         im_fibra = np.array(255*im_fibra, dtype='uint8')
         imagenes.append(im_fibra)
         
     return imagenes, splines
-        
-        
         
