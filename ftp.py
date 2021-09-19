3a7ad418d9068a4b0316e21d28aad0652c41683f
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-09-19 18:28:48 -0300

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import signal
+import scipy.fft as scf
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 # The following line requires "unwrap" to be installed (this is a very robust 2D unwrapper)
@@ -111,6 +112,122 @@ if __name__ == "__main__":
     plt.colorbar()
     plt.show()
 #%%
+def dphase_2d(dY0,dY,thx,thy,ns):
+    ny, nx = np.shape(dY)
+    
+    fY0=np.fft.fft2(dY0)
+    fY=np.fft.fft2(dY)
+    
+    inde = 9
+    a = np.abs(fY0[inde:int(np.floor(nx/2)),:])
+    imax = np.unravel_index(np.argmax(a, axis=None), a.shape)
+    ifmax_x, ifmax_y = imax[1]+inde, 0
+    print(ifmax_x, ifmax_y)
+                           
+    HW_x, HW_y = np.round(ifmax_x*thx), np.round(thy)
+    W_x, W_y = 2*HW_x, 2*HW_y
+    win_x, win_y = signal.tukey(int(W_x),ns), signal.tukey(int(W_y),ns)
+    wxx, wyy = np.meshgrid(win_x,win_y)
+    win = wxx * wyy
+    
+    
+    gaussfilt1D = np.zeros((nx,ny))
+    gaussfilt1D[int(ifmax_y-HW_y):,
+                int(ifmax_x-HW_x-1):int(ifmax_x-HW_x+W_x-1)] = win[:int(ifmax_y-HW_y+W_y),:]
+    gaussfilt1D[:int(ifmax_y-HW_y+W_y),
+                int(ifmax_x-HW_x-1):int(ifmax_x-HW_x+W_x-1)] = win[int(ifmax_y-HW_y):,:]
+    
+    # Multiplication by the filter
+    Nfy0 = fY0*gaussfilt1D
+    Nfy = fY*gaussfilt1D
+    
+    # Inverse Fourier transform of both lines
+    Ny0 = np.fft.ifft2(Nfy0)
+    Ny  = np.fft.ifft2(Nfy)
+    # 
+    phase0 = np.angle(Ny0)
+    phase = np.angle(Ny)
+    
+    mphase0 = unwrap(phase0)
+    mphase  = unwrap(phase)
+    
+    dp = mphase0 - mphase
+    dp = dp - dp[0,0]
+    return dp, gaussfilt1D, fY0
+
+
+#%%
+dp, gf, fyo = dphase_2d(im_def,im_ref,0.5,250,0.2)
+#dp, gf, fyo, inde2 = dphase_2d_2(im_ref,im_def,0.5,0.2)
+#dph, inde1 = calculate_phase_diff_map_1D(im_def, im_ref, 0.5, 0.2)
+
+#fir = scf.fft2(im_def)
+
+#plt.figure()
+#plt.imshow(im_def)
+#plt.colorbar()
+#plt.show()
+
+plt.figure()
+plt.imshow(phase_imposed)
+plt.title("phase imposed (ground truth)")
+plt.colorbar()
+plt.show()
+# 
+plt.figure()
+plt.imshow(dp)
+plt.colorbar()
+plt.show()
+
+plt.figure()
+plt.title("error = abs( recovered phase map - phase imposed)")
+plt.imshow(np.abs(dp - phase_imposed))
+plt.colorbar()
+plt.show()
+
+    
+#plt.figure()
+#plt.imshow(gf)
+#plt.colorbar()
+#plt.show()
+#
+#bx,by = 25,255
+#lx,ly = 25,255
+##x, y = np.linspace(bx-lx,bx+lx,2*lx+1), np.linspace(by-ly,by+ly,2*ly+1)
+#fx = scf.fftfreq(512)
+#fy = scf.fftfreq(512)
+#FX,FY = np.meshgrid(fx,fy)
+##
+##fyt = np.abs(fyo[bx-lx:bx+lx+1,by-ly:by+ly+1])
+##
+#fig = plt.figure()
+#ax = fig.gca(projection='3d')
+#ax.plot_surface(FX[:,:100], FY[:,:100], np.abs(fir[:,:100]),cmap='jet')
+##ax.plot_surface(X, Y, fyt)
+#ax.set_xlabel('x')
+#ax.set_ylabel('y')
+#plt.show()
+##
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_wireframe(FX[:,:100], FY[:,:100], gf[:,:100],cmap='jet')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.show()
+#
+#ffi = np.abs(fir) * gf
+#fig = plt.figure()
+#ax = fig.gca(projection='3d')
+#ax.plot_surface(FX[:,:100], FY[:,:100], ffi[:,:100],cmap='jet')
+##ax.plot_surface(X, Y, fyt)
+#ax.set_xlabel('x')
+#ax.set_ylabel('y')
+#plt.show()
+
+
+
+
+#%%
 im_p = np.zeros((1024,1024))
 for i in range(1,301):
     ni = '{:04d}'.format(i)
@@ -149,123 +266,27 @@ plt.imshow(recovered_phase_map)
 plt.colorbar()
 plt.show()
 
-recovered_phase_map = calculate_phase_diff_map_1D_2(im-im_p, imr-im_p, 0.2, 0.2)
-recovered_phase_map = recovered_phase_map - recovered_phase_map[0,0] 
-plt.figure()
-plt.title("recovered phase map")
-plt.imshow(recovered_phase_map)
-plt.colorbar()
-plt.show()
+#%%
+dp, gafi, fY0 = dphase_2d(im-im_p, imr-im_p,0.2,510,0.2)
+
+fx = scf.fftfreq(1024)
+fy = scf.fftfreq(1024)
+FX,FY = np.meshgrid(fx,fy)
 #
-#recovered_phase_map = calculate_phase_diff_map_1D(im-im_p, imr-im_p, 0.3, 0.3)
-#recovered_phase_map = recovered_phase_map - recovered_phase_map[0,0] 
-#plt.figure()
-#plt.title("recovered phase map")
-#plt.imshow(recovered_phase_map)
-#plt.colorbar()
-#plt.show()
-#%%
-def calculate_phase_diff_map_1D_2(dY, dY0, th, ns):
-    ny, nx = np.shape(dY)
-    phase0 = np.zeros([nx,ny])
-    phase  = np.zeros([nx,ny])
-
-    dfy=1./ny
-    fy=np.arange(dfy,1,dfy)
-
-    fY0 = np.fft.fft2(dY0)
-
-    for lin in range (0,nx):
-        fY0=np.fft.fft(dY0[lin,:])
-        fY=np.fft.fft(dY[lin,:])
-
-        imax=np.argmax(np.abs(fY0[9:int(np.floor(nx/2))]))
-        ifmax=imax+9
-
-        HW=np.round(ifmax*th)
-        W=2*HW
-        win=signal.tukey(int(W),ns)
-
-        gaussfilt1D = np.zeros(nx)
-        gaussfilt1D[int(ifmax-HW-1):int(ifmax-HW+W-1)] = win
-
-        # Multiplication by the filter
-        Nfy0 = fY0*gaussfilt1D
-        Nfy = fY*gaussfilt1D
-
-        # Inverse Fourier transform of both lines
-        Ny0 = np.fft.ifft(Nfy0)
-        Ny  = np.fft.ifft(Nfy)
- 
-        phase0[lin,:] = np.angle(Ny0)
-        phase[lin,:]  = np.angle(Ny)
-
-        phase0[lin,:] = phase0[lin,:] - phase0[lin,0] + phase0[0,0]
-        phase[lin,:]  = phase[lin,:] - phase[lin,0] + phase[0,0] 
-    
-    mphase0 = unwrap(phase0)
-    mphase  = unwrap(phase)
-    
-    # Definition of the phase difference map
-    dphase = (mphase-mphase0)
-    return dphase
-#%%
-def dphase_2d(dY0,dY,thx,thy,ns):
-    ny, nx = np.shape(dY)
-    
-    fY0=np.fft.fft2(dY0)
-    fY=np.fft.fft2(dY)
-    
-    inde = 40
-    a = np.abs(fY0[inde:int(np.floor(nx/2)),inde:int(np.floor(ny/2))])
-    imax = np.unravel_index(np.argmax(a, axis=None), a.shape)
-    ifmax_x, ifmax_y = imax[0]+inde, imax[1]+inde
-    print(ifmax_x, ifmax_y)
-                           
-    HW_x, HW_y = np.round(ifmax_x*thx), np.round(ifmax_y*thy)
-    W_x, W_y = 2*HW_x, 2*HW_y
-    win_x, win_y = signal.tukey(int(W_x),ns), signal.tukey(int(W_y),ns)
-    wxx, wyy = np.meshgrid(win_x,win_y)
-    win = wxx * wyy
-    
-    gaussfilt1D = np.zeros((nx,ny))
-    gaussfilt1D[int(ifmax_y-HW_y-1):int(ifmax_y-HW_y+W_y-1),
-                         int(ifmax_x-HW_x-1):int(ifmax_x-HW_x+W_x-1)] = win
-    
-    # Multiplication by the filter
-    Nfy0 = fY0*gaussfilt1D
-    Nfy = fY*gaussfilt1D
-    
-    # Inverse Fourier transform of both lines
-    Ny0 = np.fft.ifft2(Nfy0)
-    Ny  = np.fft.ifft2(Nfy)
-    # 
-    phase0 = np.angle(Ny0)
-    phase = np.angle(Ny)
-    
-    mphase0 = unwrap(phase0)
-    mphase  = unwrap(phase)
-    
-    dp = mphase - mphase0
-    dp = dp - dp[0,0]
-    return dp, gaussfilt1D, fY0
-#%%
-dp, gafi, fY0 = dphase_2d(im-im_p, imr-im_p,0.2,0.2,0.2)
-bx,by = 512, 256
-lx,ly = 10,5
-x, y = np.linspace(bx-lx,bx+lx,2*lx+1), np.linspace(by-ly,by+ly,2*ly+1)
-X,Y = np.meshgrid(y,x)
-
-fyt = np.abs(fY0[bx-lx:bx+lx+1,by-ly:by+ly+1])
-#
-plt.figure()
-plt.imshow(gafi)
-plt.colorbar()
-plt.show()
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.plot_surface(X, Y, fyt)
+ax.plot_wireframe(FX[:,:100], FY[:,:100], gafi[:,:100],cmap='jet')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
 plt.show()
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_wireframe(FX[:,:100], FY[:,:100], np.abs(fY0[:,:100]))
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.show()
+
 plt.figure()
 plt.imshow(dp)
 plt.colorbar()
