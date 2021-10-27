49211413c11c38707fdd4094055000e60f828bb8
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-10-27 17:45:06 -0300

    Update Analisis.py

--- Analisis.py
+++ Analisis.py
@@ -96,7 +96,10 @@ with h5py.File('ftp.hdf5', 'w') as f:
     
 #%%
 dps, splif = [],[]
-with h5py.File('splines.hdf5', 'r') as f:  
+spli_hdf = r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Labo\Analisado\splines.hdf5'
+ftp_hdf = r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Labo\Analisado\ftp.hdf5'
+
+with h5py.File(spli_hdf, 'r') as f:  
     gspf = f.get('splines_recons')
     tf_h, kf_h = gspf['lista_splf_t'], gspf['lista_splf_k']
     c1f_h, c2f_h = gspf['lista_splf_c1'], gspf['lista_splf_c2'] 
@@ -104,7 +107,7 @@ with h5py.File('splines.hdf5', 'r') as f:
     for i in range(len(tf_h)):
 #        imag.append(im_h[i])
         splif.append([tf_h[i],[c1f_h[i],c2f_h[i]],kf_h[i]])
-with h5py.File('ftp.hdf5', 'r') as f:
+with h5py.File(ftp_hdf, 'r') as f:
     gdp = f.get('dif_phase')
     h_dp = gdp['dp']
     for i in range(len(h_dp)):
@@ -122,34 +125,62 @@ plt.plot(xf-lim, yf-lim, 'r-')
 plt.colorbar()
 plt.show()
 #%%
-num = 51 #47,51
+# Para hacer una imagen sola
+num = 324 #47,51
 # 47 no funciona
 ni = '{:04d}'.format(num)
 im = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Labo\FTP_fibra\ID_0_C1S000500'+ni+'.tif')
 im = np.array(im,dtype='float')
 
-mul = 2.0
+mul = 2.46
 fibra, bb, fib_an = spl.recupera_fibra(im,gris,std_mul=mul)
 tramos,bordes = spl.cortar_fibra_rap(fibra,bb,cortar_ruido=True)
 tramos = spl.ordenar_fibra(tramos)
 try:
     curv,spline = spl.pegar_fibra(tramos,bordes,window=15,s=16)
-    splines.append(spline)
 except: print('Jajaja', num)
-#tm = time()
-#t_spl = np.linspace(0, 1, 10000)
-#xf, yf = splev(t_spl, spline)
 
 imfc, ffc = cf.hacer_fou_cont(im,fibra,bb,ventana=150,mmx=2,mmy=2)
-#tm2 = time()
 thx,thy, ns = 0.5, 80, 0.5
 dph, ft, gf = cf.dphase_2d(imfc-gris,ref-gris,thx,thy,ns,inde=9)
-dphs.append(dph)
 #%%
-plt.figure()
-plt.imshow(fibra)
-plt.show()
+t_spl = np.linspace(0, 1, 10000)
+xf, yf = splev(t_spl, spline)
+ventana = 150
+bb1,bb2 = int((bb[0]+bb[2])/2), int((bb[1]+bb[3])/2)
+vent = int(ventana/2)
+b1,b2,b3,b4 = bb1-vent,bb1+vent,bb2-vent,bb2+vent
+#im_nf = im[bb[0]:bb[2], bb[1]:bb[3]] * (1-fib_an)
+#im_nf[im_nf==0] = np.nan
+#def_hole = np.zeros(np.shape(im))
+#def_hole += im
+#def_hole[bb[0]:bb[2], bb[1]:bb[3]] = im_nf
+#def_hole = def_hole[b1:b2,b3:b4] 
 
+#%%
+from skimage.filters import laplace, sobel
+plt.figure()
+imlap = sobel(laplace(im-0.8*gris,ksize=3),mode='wrap')
+binariza = np.mean(imlap) + 2.46*np.std(imlap)
+imb = imlap>binariza
+plt.imshow(imlap[b1:b2,b3:b4],cmap='gray')
+#plt.imshow(laplace(im-0.8*gris,ksize=3),cmap='gray')
+#plt.imshow(imb,cmap='gray')
+#plt.plot(xf,yf,'r-')
+plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
+#plt.colorbar()
+plt.savefig('imageni.png',bbox_inches='tight')  
+plt.show()
+#%%
+t_spl = np.linspace(0, 1, 10000)
+xf, yf = splev(t_spl, spline)
+plt.figure()
+plt.imshow(im,cmap='gray')
+plt.plot(xf,yf,'r-')
+plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
+plt.savefig('imageni.png',bbox_inches='tight')  
+plt.show()
+#%%
 t_spl = np.linspace(0, 1, 10000)
 xf, yf = splev(t_spl, spline)
 plt.figure()
