9da29e8820c7e16a29493e26968cc3b9caf5ad5c
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-11-08 21:47:21 -0300

    Update intento_energia.py

--- intento_energia.py
+++ intento_energia.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import Func_Splines as spl
+import Continuacion_Fourier as cf
+from skimage.io import imread
+from scipy.interpolate import splev
 import h5py 
 #%%
 dps = []
@@ -12,6 +16,9 @@ with h5py.File(ftp_hdf, 'r') as f:
 #%%
 plt.figure()
 plt.imshow(dps[0][300:900,300:900])
+plt.show()
+plt.figure()
+plt.imshow(dps[0])
 plt.show()
 #%%
 nt, nxy = 200, 600
@@ -74,3 +81,46 @@ plt.loglog(kx[:100], np.abs(dpft[0,:100,0]))
 plt.loglog(kx[:100], lin)
 plt.grid()
 plt.show()
+
+#%%
+#-------------------------------------------------------------------------------------------
+#%%
+gris = np.zeros((1024,1024))
+for i in range(1,301):
+    ni = '{:04d}'.format(i)
+    ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Labo\Grises\ID_0_C1S000600'+ni+'.tif')
+    ima = np.array(ima,dtype='float')
+    gris += ima
+gris = gris/300
+
+ref = np.zeros((1024,1024))
+for i in range(1,301):
+    ni = '{:04d}'.format(i)
+    ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Labo\Referencia\ID_0_C1S000300'+ni+'.tif')
+    ima = np.array(ima,dtype='float')
+    ref += ima
+ref = ref/300
+#%%
+num = 10
+ni = '{:04d}'.format(num)
+im = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Labo\FTP_fibra\ID_0_C1S000500'+ni+'.tif')
+im = np.array(im,dtype='float')
+mul = 2.46
+if num == 51: mul = 2
+#elif num == 47: continue
+fibra, bb, fib_an = spl.recupera_fibra(im,gris,std_mul=mul)
+imfc, ffc = cf.hacer_fou_cont(im,fibra,bb,ventana=150,mmx=2,mmy=2)
+thx,thy, ns = 0.25, 45, 0.75
+dph, ft, gf = cf.dphase_2d(imfc-gris,ref-gris,thx,thy,ns,inde=9)
+#%%
+lim = 15
+plt.figure()
+plt.imshow(dph[lim:-lim,lim:-lim])
+#plt.imshow(dph[300:900,300:900])
+plt.title('thx = '+str(thx)+', thy = '+str(thy)+', ns = '+str(ns))
+#plt.imshow(dph)
+plt.colorbar()
+plt.show()
+
+
+
