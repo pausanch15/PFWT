0c24a011911d2945a1112efdf3f0022dbbedad94
pausanch15 <paulijsanchez@live.com.ar>
2021-12-18 11:18:48 -0300

    Ahora si, hace los histogramas

--- histogramas_vya.py
+++ histogramas_vya.py
@@ -7,114 +7,46 @@ from skimage.io import imread
 from scipy.interpolate import splev
 from time import time
 import h5py 
+from tqdm import tqdm
 plt.ion()
 
 #%%
-#Traigo imagenes
-gris = np.zeros((1024, 1024))
-for i in range(1, 16):
-    ni = '{:04d}'.format(i)
-    ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
-    # ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0003\ID_0_C1S000300'+ni+'.tif')
-    ima = np.array(ima, dtype='float')
-    gris += ima
-gris = gris/15
+#Traigo los hdf5 con las alturas y hago el histograma de las alturas
+num_bins = 10000
+binis = np.linspace(-num_bins/2, num_bins/2, num_bins)
+H = np.zeros(num_bins-1)
 
-ref = np.zeros((1024, 1024))
-for i in range(1, 14):
-    ni = '{:04d}'.format(i)
-    ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0002/ID_0_C1S000300'+ni+'.tif')
-    # ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0002\ID_0_C1S000300'+ni+'.tif')
-    ima = np.array(ima, dtype='float')
-    ref += ima
-ref = ref/13
-
-#%%
-#Para pasar de diferencia de fase a altura, necesitamos la longitud de onda del patrón de rayas
-from scipy.signal import find_peaks
-asd = ref - gris
-xp = np.arange(0,1024,1)
-x = 0.02086 * xp
-n = 200
-lin = asd[n,:] - np.mean(asd[n,:])
-
-a, _ = find_peaks(lin,distance=10) #hay 82 picos, o sea, 81 longitudes de onda
-d = (1014-9) / 81 * 0.02086
-w = 2*np.pi/d
-rec = np.cos(w*x + 1.5) * 75
-
-#%%
-#Saco altura para las imagenes que tienen la fibra
-#Hago ftp y guardo en hf5 las alturas
-thx,thy, ns = 0.25, 45, 0.75 #0.5, 80, 0.5
-
-#Defino los valores de L y D (los medimos en el labo)
-w = 2*np.pi/d
-L, D = 79.6, 20.3
-
-# dphs = np.zeros((3072,1024,1024))
-with h5py.File('18-12-2021_ftp.hdf5', 'w') as f:
-    h_im = f.create_group('hs')
-    alt = h_im.create_dataset('alts', shape=(150,1024,1024))
-    for num, i in zip(range(1, 151), tqdm(range(1, 151))):
-        ni = '{:04d}'.format(num)
-        ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0001/ID_0_C1S000100'+ni+'.tif')
-        # ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
-        im = np.array(ima,dtype='float')
-        dph, ft, gf = cf.dphase_2d(im,ref-gris,thx,thy,ns,inde=9)
-        altura = (L*dph) / (dph - w*D)
-        alt[num-1] = altura - np.mean(altura)
-        del(ni, ima, im, altura)
-
-#%%
-#Traigo los hdf5
-alturas = []
-ftp_hdf = r'/home/paula/Documents/Fisica2021/L6y7/PFWT/18-12-2021_ftp.hdf5'
-
-with h5py.File(ftp_hdf, 'r') as f:
+with h5py.File('18-12-2021_ftp.hdf5', 'r') as f:
     gdp = f.get('hs')
     h_dp = gdp['alts']
-    for i in range(len(h_dp)):
-        alturas.append(h_dp[i])
+    for i, j in zip(range(1,len(h_dp)), tqdm(range(1,len(h_dp)))):
+        hs = (h_dp[i][10:-10, 10:-10]).flatten()
+        his, bins = np.histogram(hs, bins=binis)
+        H += his
 
-#%%
-#Ahora los hsitogramas
-#Primero el de las alturas
-h_un_frame = 1024*1024 #Cantidad dealturas por frame
-n_frames = len(alturas)
-hs = np.zeros(h_un_frame*n_frames) #array que va a tener la velocidad de cada punto de todos los frames
-
-for i, im in enumerate(alturas):
-    i = i + 1
-    if i == len(alturas):
-        break
-    h = im.flatten()
-    inic =  (i-1)*h_un_frame
-    fin = i*h_un_frame
-    hs[inic:fin] = h
-    del(h, inic, fin)
-
-plt.hist(hs, bins=100, density=True, edgecolor="black")
+norm = np.sum(H)
+plt.figure()
+plt.bar(binis[:-1], height=H/norm, width=binis[1]-binis[0], edgecolor='black')
 plt.grid()
 plt.title('Histograma de Alturas')
 plt.show()
 
-#Ahora de las velocidades
-v_un_frame = np.shape((alturas[1] - alturas[0]).flatten())[0] #Cantidad de velocidades por frame
-n_frames = len(alturas)
-v = np.zeros(v_un_frame*n_frames) #array que va a tener la velocidad de cada punto de todos los frames
+#%%
+#Ahora el de las alturas
+with h5py.File(ftp_hdf, 'r') as f:
+    gdp = f.get('dif_phase')
+    h_dp = gdp['dp']
+    for i, j in zip(range(1,len(h_dp)), tqdm(range(1,len(h_dp)))):
+        dp1 = h_dp[i][10:-10,10:-10]
+        dp0 = h_dp[i-1][10:-10,10:-10]
+        vel = (250*(dp1-dp0)).flatten()
+        his, bins = np.histogram(vel,bins=binis)
+        H += his
 
-for i, im in enumerate(alturas):
-    i = i + 1
-    if i == len(alturas):
-        break
-    velocidad = (250*(alturas[i]-im)).flatten()
-    inic =  (i-1)*v_un_frame
-    fin = i*v_un_frame
-    v[inic:fin] = velocidad
-    del(velocidad, inic, fin)
-
-plt.hist(v, bins=100, density=True, edgecolor="black")
+norm = np.sum(H)
+plt.figure()
+plt.bar(binis[:-1],H/norm,width=binis[1]-binis[0],edgecolor='black')
+plt.show()
 plt.grid()
 plt.title('Histograma de Velocidades')
 plt.show()
@@ -123,15 +55,18 @@ plt.show()
 a = np.zeros(v_un_frame*n_frames)
 
 for i in range(1, len(alturas)-1):
-    aceleracion = (250**2)*(alturas[i+1]-2*alturas[i]+alturas[i-1])
+    dp1 = h_dp[i][10:-10,10:-10]
+    dp0 = h_dp[i-1][10:-10,10:-10]
+    dp2 = h_dp[i+1][10:-10,10:-10]
+    aceleracion = (250**2)*(dp2-2*dp0+dp1)
     aceleracion = aceleracion.flatten()
-    inic =  (i-1)*v_un_frame
-    fin = i*v_un_frame
-    a[inic:fin] = aceleracion
+    his, bins = np.histogram(aceleracion, bins=binis)
     del(aceleracion, inic, fin)
 
 #Hago el histograma de aceleración
-plt.hist(v, bins=100, density=True, edgecolor="black")
+norm = np.sum(H)
+plt.figure()
+plt.bar(binis[:-1],H/norm,width=binis[1]-binis[0],edgecolor='black')
 plt.grid()
 plt.title('Histograma de Aceleración')
 plt.show()
