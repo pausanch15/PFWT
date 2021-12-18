0792f3ff24168355716f2daff56c7df50ac10879
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-12-18 14:22:56 -0300

    Update Analisis_Fibra2.py

--- Analisis_Fibra2.py
+++ Analisis_Fibra2.py
@@ -4,22 +4,22 @@ import Func_Splines as spl
 from skimage.io import imread
 from scipy.interpolate import splev
 from time import time
-from plantcv import plantcv as pcv
+#from plantcv import plantcv as pcv
 #import h5py 
 plt.ion()
 #%%
 from scipy.signal import convolve2d, medfilt
 from skimage.filters import gaussian, frangi, sato, gabor, laplace, sobel, roberts, hessian, inverse
-from skimage.filters.rank import minimum, enhance_contrast, gradient
+from skimage.filters.rank import minimum, enhance_contrast, gradient, maximum, mean_bilateral, mean
 from skimage.morphology import disk, flood, flood_fill
 from skimage.measure import label, regionprops
 from skimage.util import img_as_ubyte
 from skimage.exposure import adjust_gamma, adjust_log
 from skimage.morphology import thin, skeletonize, remove_small_objects, dilation, binary_erosion, binary_closing, closing
 #%%
-num = 1500
+num = 2750
 ni = '{:04d}'.format(num)
-ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0007\ID_0_C1S000700'+ni+'.tif')
+ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
 #ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
 im = np.array(ima,dtype='float')
 
@@ -28,9 +28,9 @@ imr = im[0:370,850:950]
 plt.figure()
 plt.imshow(im)
 plt.show()
-plt.figure()
-plt.imshow(imr)
-plt.show()
+#plt.figure()
+#plt.imshow(imr)
+#plt.show()
 #%%
 def encontrar_fibra2(im,bb,prun=20,std_mul=2,disco=7,mulm=0.8,tama=145,tipog='wrap'):
     imr = im[bb[0]:bb[1],bb[2]:bb[3]]
@@ -151,7 +151,8 @@ ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Bueno
 im = np.array(ima,dtype='float')
 
 bbns = {800:[0,60,250,550], 819:[0,70,220,550], 1242:[0,280,450,650], 824:[0,122,224,524],
-828:[0,140,234,532], 760:[0,100,220,510], 1800:[210,440,570,810], 1243:[0,280,450,650] }
+828:[0,140,234,532], 760:[0,100,220,510], 1800:[210,440,570,810], 1243:[0,280,450,650],
+1308:[0, 70, 220, 550]}
 bb = bbns[num]
 imr = im[bb[0]:bb[1],bb[2]:bb[3]]
 
@@ -239,13 +240,13 @@ mjk = frangi(-gaussian(erw,5))
 #plt.colorbar()
 #plt.show()
 plt.figure()
-plt.imshow( frangi(df) )
+plt.imshow( gradient(df/np.min(df),disk(3)) )
 plt.colorbar()
 plt.show()
-plt.figure()
-plt.imshow( (mjk/np.max(mjk))   )
-#plt.colorbar()
-plt.show()
+#plt.figure()
+#plt.imshow( (mjk/np.max(mjk))   )
+##plt.colorbar()
+#plt.show()
 #plt.figure()
 #plt.imshow( df )
 #plt.show()
@@ -403,4 +404,376 @@ plt.figure()
 plt.imshow(im)
 plt.plot(xf, yf, 'r-')
 plt.show()
+#%%
+#==============================================================================
+# Lo que dijimos en el labo, con lo nuevo 
+#==============================================================================
+def encontrar_fib(im,bb,disc=3):
+    imr = im[bb[0]:bb[1],bb[2]-20:bb[3]+20]
+    a1 = imr - gaussian(imr,20)
+    a = gabor(a1,0.1)
+    b = a1 - 2*(a[0]-np.min(a[0]) + 10)
+    c = b - gaussian(b,30,mode='mirror')
+    d = gaussian(c,0.5)
+    
+    th = gabor(d*(d<0),0.7)
+    ttt = th[1]+th[0]
+    
+    ggg = gaussian(ttt[:,20:-20],3) - gaussian(ttt[:,20:-20],5)
+    hi = frangi(ggg)
+    mii = minimum( img_as_ubyte(hi),np.array([[0,0,0],
+                                              [1,1,1],
+                                              [0,0,0]]))
+    rso = remove_small_objects(mii>0,min_size=20)
+    dso = dilation(rso,disk(disc))
+    lal = label(dso)
+    fib = skeletonize(rso)
+    
+    lfib = np.where(fib) 
+    yma, xma = np.max(lfib[0]),np.max(lfib[1])
+    ymi, xmi = np.min(lfib[0]),np.min(lfib[1])
+    
+    xmax, ymax = min(1023,xma+bb[2]+1), min(1023,yma+bb[0]+1) 
+    xmin, ymin = max(0,xmi+bb[2]), max(0,ymi+bb[0])
+    bbs = [ymin,xmin,ymax,xmax]
+    fii = fib[ymi:yma+1,xmi:xma+1]
+    
+    return fii>0, imr, bbs, np.max(lal)
+
+def bordes_reales(tramos,bordes,imprimir=False):
+    if len(tramos) > 1 and len(bordes) > 2:
+        if imprimir: print('n° tramos:',len(tramos),'\nn° bordes:',len(bordes))
+        d0 = 0
+        for i in range(len(bordes)):
+            for j in range(i+1,len(bordes)):
+                b1 = np.array(bordes[i])
+                b2 = np.array(bordes[j])
+                dist = np.sum((b1-b2)**2)
+                if dist > d0:
+                    n,m = i,j
+                    d0 = dist
+        bo = np.array([bordes[n],bordes[m]])
+        return bo
+    else:
+        if imprimir: print('n° tramos:',len(tramos),'\nn° bordes:',len(bordes))
+        bo = bordes
+        return bo
+#%%
+t1 = time()
+splines = []
+bb = [0,100,220,510] #para 760
+buenas, malas = [], []
+bbt = [bb]
+for num in range(760,900):
+    ni = '{:04d}'.format(num)
+    ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
+    #ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
+    im = np.array(ima,dtype='float')
+    
+    if num%30 == 0: print(num,end=' ')
+    
+    try:   
+        disc=3
+        if num == 770: disc=2
+        
+        fib,imr,bbn,nl = encontrar_fib(im,bb,disc=disc)
+        
+        if nl > 2:
+            malas.append(num)
+            splines.append('No')
+            bbt.append(bb)
+            continue
+        
+        buenas.append(num)
+        tramos,bordes = spl.cortar_fibra_rap(fib,bbn,cortar_ruido=True)
+        bo = bordes_reales(tramos,bordes,imprimir=False)
+        tramos = spl.ordenar_fibra(tramos)
+        curv,spline = spl.pegar_fibra(tramos,bo,window=27,s=25)
+
+    except:
+        print('Fallo',num)
+        break
+    
+    splines.append(spline)
+    
+    nb1, nb2, = max(0,bbn[0]-40), min(1024,bbn[2]+40)
+    nb3, nb4 = max(0,bbn[1]-40), min(1024,bbn[3]+40)
+    bb = [nb1,nb2,nb3,nb4]
+    bbt.append(bb)
+    
+t2 = time()
+print(t2-t1)
+#%%
+malas, buenas
+#%%
+n = -760+761
+print(bbt[n])
+ni = '{:04d}'.format(760+n)
+ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
+#ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
+im = np.array(ima,dtype='float')
+
+    
+t_spl = np.linspace(0,1,100000)
+xf, yf = splev(t_spl, splines[n])
+
+plt.figure()
+plt.imshow(im)
+plt.colorbar()
+plt.show()
+plt.figure()
+plt.imshow(im)
+plt.plot(xf, yf, 'r-')
+plt.show()
+#%%
+num = 2589
+
+ni = '{:04d}'.format(num)
+ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
+#ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
+im = np.array(ima,dtype='float') 
+
+bbns = {800:[0,60,250,550], 819:[0,70,220,550], 1242:[0,280,450,650], 824:[0,122,224,524],
+828:[0,140,234,532], 760:[0,100,220,510], 1800:[210,440,570,810], 1243:[0,280,450,650],
+1308:[0, 300, 450, 650], 911:[0,197,326,627], 965:[0,257,364,597]}
+
+#bb = bbns[num]
+bb = bbt[num-2550]
+#bb = bbt[-1]
+#bb = [650,850,650,900]
+
+imr = im[bb[0]:bb[1],bb[2]-20:bb[3]+20]
+
+plt.figure()
+plt.imshow(imr)
+plt.show()
+#%%
+a1 = imr - gaussian(imr,20)
+a = gabor(a1,0.1)
+b = a1 - 2*(a[0]-np.min(a[0]) + 10)
+c = b - gaussian(b,30,mode='mirror')
+d = gaussian(c,0.5)
+
+th = gabor(d*(d<0),0.7)
+ttt = th[1]+th[0]
+
+ggg = gaussian(ttt,3,mode='mirror') - gaussian(ttt,4,mode='mirror')
+hi = sato(ggg[:,20:-20],mode='reflect')
+hii = hi / np.max(hi)
+n = 5
+ker = np.zeros((n,n))
+ker[int(n/2),:] = 1
+mii = minimum( img_as_ubyte(hii),ker)
+maa = maximum( img_as_ubyte(hii),disk(3))
+mea = mean( img_as_ubyte(hii),ker) 
+fgi = gaussian(mii,3)
+#rso = remove_small_objects(mii>0,min_size=20)
+#dso = dilation(rso,disk(2))
+#lal = label(dso)
+#fib = skeletonize(rso)
+
+gmi = gaussian(mii,3) - gaussian(mii,4)
+gma = gaussian(maa,5) - gaussian(maa,6)
+
+fmi = frangi(-gmi)
+fma = frangi(-gma)
+
+fi3 = (fmi/np.max(fmi))**3
+fa3 = (fma/np.max(fma))**3 
+ma,sa = np.mean(fa3), np.std(fa3)
+mi,si = np.mean(fi3), np.std(fi3)
+print(mi,si)
+rmo = remove_small_objects(fi3 > si/3 , min_size=180)
+rma = remove_small_objects(fa3 > sa/3 , min_size=310)
+
+plt.figure()
+plt.imshow( rmo )
+#plt.colorbar()
+plt.show()
+plt.figure()
+plt.imshow( rma )
+#plt.colorbar()
+plt.show()
+#plt.figure()
+#plt.imshow( mea )
+##plt.colorbar()
+#plt.show()
+#%%
+#==============================================================================
+# Hago con lo nuevo de arriba 
+#==============================================================================
+def encontrar_fibn(im,bb,nk=5,mima='min',divi=3,size=15):
+    imr = im[bb[0]:bb[1],bb[2]-20:bb[3]+20]
+    a1 = imr - gaussian(imr,20)
+    a = gabor(a1,0.1)
+    b = a1 - 2*(a[0]-np.min(a[0]) + 10)
+    c = b - gaussian(b,30,mode='mirror')
+    d = gaussian(c,0.5)
+    
+    th = gabor(d*(d<0),0.7)
+    ttt = th[1]+th[0]
+    
+    ggg = gaussian(ttt,3,mode='mirror') - gaussian(ttt,4,mode='mirror')
+    hi = sato(ggg[:,20:-20],mode='reflect')
+    hii = hi / np.max(hi)
+    n = nk
+    ker = np.zeros((n,n))
+    ker[int(n/2),:] = 1
+    if mima == 'min':
+        mii = minimum( img_as_ubyte(hii),ker)
+        gmi = gaussian(mii,3) - gaussian(mii,4)
+        fmi = frangi(-gmi)
+        fi3 = (fmi/np.max(fmi))**3
+        si = np.std(fi3)
+    elif mima == 'max':
+        mii = maximum( img_as_ubyte(hii),disk(3))
+        gmi = gaussian(mii,5) - gaussian(mii,6)
+        fmi = frangi(-gmi)
+        fi3 = (fmi/np.max(fmi))**3
+        si = np.std(fi3)
+        
+    rmo = remove_small_objects(fi3 > si/divi , min_size=size)
+    fib = skeletonize( rmo )
+    
+    lfib = np.where(fib) 
+    yma, xma = np.max(lfib[0]),np.max(lfib[1])
+    ymi, xmi = np.min(lfib[0]),np.min(lfib[1])
+    
+    xmax, ymax = min(1023,xma+bb[2]+1), min(1023,yma+bb[0]+1) 
+    xmin, ymin = max(0,xmi+bb[2]), max(0,ymi+bb[0])
+    bbs = [ymin,xmin,ymax,xmax]
+    fii = fib[ymi:yma+1,xmi:xma+1]
+    
+    return fii>0, imr, bbs
+
+def bordes_reales(tramos,bordes,imprimir=False):
+    if len(tramos) > 1 and len(bordes) > 2:
+        if imprimir: print('n° tramos:',len(tramos),'\nn° bordes:',len(bordes))
+        d0 = 0
+        for i in range(len(bordes)):
+            for j in range(i+1,len(bordes)):
+                b1 = np.array(bordes[i])
+                b2 = np.array(bordes[j])
+                dist = np.sum((b1-b2)**2)
+                if dist > d0:
+                    n,m = i,j
+                    d0 = dist
+        bo = np.array([bordes[n],bordes[m]])
+        return bo
+    else:
+        if imprimir: print('n° tramos:',len(tramos),'\nn° bordes:',len(bordes))
+        bo = bordes
+        return bo
+#%%
+t1 = time()
+splines = []
+#bb = [0,100,220,510] #para 760, corre hasta 1100
+#bb = [0,274,411,659] #para 1100, corre hasta 1300 
+#bb = [0,305,484,656] #para 1300, corre hasta 1700 (algunas quedan medio cortas, no mucho)
+# entre 1150 y 1600 aprox, la fibras son muy onduladas (tal vez con un dilation se arregle)
+#bb = [201,474,558,820] #para 1700, corre hasta 2100
+#bb = [332, 552, 525, 835] #para 2100, hasta 2531 (pareciera haber alguna ondulaciones)
+#entre 2531 y 2550 no pude distinguir la fibra de forma rapida
+bb = [650,850,650,900] #para 2550
+
+bbt = [bb]
+
+maxi = [1242,1243,1263,1264,1265,1266,1375]
+maxi2 = [1796,1798,1805]
+maxi3 = [1797,1875,2147]
+si0 = [2012,2014,2018,2019,2020,2199]
+
+for num in range(2550,2700):
+    ni = '{:04d}'.format(num)
+    ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
+    #ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
+    im = np.array(ima,dtype='float')
+    
+    if num%30 == 0: print(num,end=' ')
+    
+    try:   
+        sise,mima = 150, 'min'
+        if num in maxi: mima, sise = 'max', 30
+        if num in range(1300,1700): sise = 80
+        if num in range(1700,2100): sise=400
+        if num in maxi2: mima, sise = 'max', 350
+        if num in maxi3: mima, sise = 'max', 150
+        if num in si0: sise = 0
+        if num in range(2515,2531): mima, sise = 'min', 900
+        if num in range(2531,2550):
+            splines.append(None)
+            bbt.append(None)
+            continue
+        if num in range(2550,2560): mima, sise = 'min', 1000
+        if num > 2560: mima, sise = 'max', 500
+        if num == 2567: sise = 310
+        
+        fib,imr,bbn = encontrar_fibn(im,bb,size=sise,mima=mima)
+        tramos,bordes = spl.cortar_fibra_rap(fib,bbn,cortar_ruido=True)
+        bo = bordes_reales(tramos,bordes,imprimir=False)
+        tramos = spl.ordenar_fibra(tramos)
+        curv,spline = spl.pegar_fibra(tramos,bo,window=27,s=25)
+
+    except:
+        print('Fallo',num)
+        break
+    
+    splines.append(spline)
+    
+    nb1, nb2, = max(0,bbn[0]-40), min(1024,bbn[2]+40)
+    nb3, nb4 = max(0,bbn[1]-40), min(1024,bbn[3]+40)
+    bb = [nb1,nb2,nb3,nb4]
+    bbt.append(bb)
+    
+t2 = time()
+print(t2-t1)
+#%%
+def largo_fib(xf,yf):
+    nf = len(xf)
+    td = 0
+    for i in range(1,nf):
+        di = np.sqrt( (xf[i]-xf[i-1])**2 + (yf[i]-yf[i-1])**2  )
+        td += di
+    return td
+#%%
+t_spl = np.linspace(0, 1, 1000)
+nn = len(splines)
+largos = []
+plt.figure()
+for n in range(nn):
+    xf, yf = splev(t_spl, splines[n])
+#    plt.imshow(np.zeros_like(im))
+    plt.plot(xf-np.mean(xf),-(yf-np.mean(yf)))
+    lar = largo_fib(xf,yf)
+    largos.append(lar)
+plt.show()
+#%%
+plt.figure()
+plt.plot(largos,'-o')
+plt.grid()
+plt.show()
+#%%
+n = -1300 + 1600
+print(bbt[n])
+ni = '{:04d}'.format(1300+n)
+ima = imread(r'C:\Users\tomfe\Documents\TOMAS\Facultad\Laboratorio 6\Datos Buenos\ID_0_C1S0001\ID_0_C1S000100'+ni+'.tif')
+#ima = imread(r'/home/paula/Documents/Fisica2021/L6y7/PFWT/ID_0_C1S0003/ID_0_C1S000300'+ni+'.tif')
+im = np.array(ima,dtype='float')
+
+    
+t_spl = np.linspace(0,1,1000)
+xf, yf = splev(t_spl, splines[n])
+
+plt.figure()
+plt.imshow(im)
+plt.colorbar()
+plt.show()
+plt.figure()
+plt.imshow(im)
+plt.plot(xf, yf, 'r-')
+plt.show()
+#plt.figure()
+#plt.imshow(fib)
+##plt.colorbar()
+#plt.show()
 #%%
