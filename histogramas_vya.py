53febd3f3f45c0216379627bc54c7e133696a00a
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-12-18 17:59:58 -0300

    Update histogramas_vya.py

--- histogramas_vya.py
+++ histogramas_vya.py
@@ -12,64 +12,83 @@ plt.ion()
 
 #%%
 #Traigo los hdf5 con las alturas y hago el histograma de las alturas
-num_bins = 10000
-binis = np.linspace(-num_bins/2, num_bins/2, num_bins)
-H = np.zeros(num_bins-1)
+num_bins = 1000
+binis = np.linspace(-5, 5, num_bins)
+Al = np.zeros(num_bins-1)
 
-with h5py.File('18-12-2021_ftp.hdf5', 'r') as f:
+#with h5py.File('18-12-2021_ftp.hdf5', 'r') as f:
+with h5py.File('alturas.hdf5', 'r') as f:
     gdp = f.get('hs')
     h_dp = gdp['alts']
     for i, j in zip(range(1,len(h_dp)), tqdm(range(1,len(h_dp)))):
         hs = (h_dp[i][10:-10, 10:-10]).flatten()
         his, bins = np.histogram(hs, bins=binis)
-        H += his
-
-norm = np.sum(H)
+        Al += his
+#%%
+norm = np.sum(Al)
 plt.figure()
-plt.bar(binis[:-1], height=H/norm, width=binis[1]-binis[0], edgecolor='black')
+plt.bar(binis[:-1], height=Al/norm, width=binis[1]-binis[0], edgecolor='black')
 plt.grid()
 plt.title('Histograma de Alturas')
 plt.show()
 
 #%%
-#Ahora el de las alturas
-with h5py.File(ftp_hdf, 'r') as f:
-    gdp = f.get('dif_phase')
-    h_dp = gdp['dp']
+#Ahora el de las velocidades
+num_bins = 1000
+binis = np.linspace(-40, 40, num_bins)
+Ve = np.zeros(num_bins-1)
+
+#with h5py.File('18-12-2021_ftp.hdf5', 'r') as f:
+with h5py.File('alturas.hdf5', 'r') as f:
+    gdp = f.get('hs')
+    h_dp = gdp['alts']
+    dp0 = h_dp[0][10:-10,10:-10]
     for i, j in zip(range(1,len(h_dp)), tqdm(range(1,len(h_dp)))):
         dp1 = h_dp[i][10:-10,10:-10]
-        dp0 = h_dp[i-1][10:-10,10:-10]
+#        dp0 = h_dp[i-1][10:-10,10:-10]
         vel = (250*(dp1-dp0)).flatten()
         his, bins = np.histogram(vel,bins=binis)
-        H += his
-
-norm = np.sum(H)
+        Ve += his
+        dp0 = dp1
+#%%
+norm = np.sum(Ve)
 plt.figure()
-plt.bar(binis[:-1],H/norm,width=binis[1]-binis[0],edgecolor='black')
+plt.bar(binis[:-1],Ve/norm,width=binis[1]-binis[0],edgecolor='black')
 plt.show()
 plt.grid()
 plt.title('Histograma de Velocidades')
 plt.show()
-
+#%%
 #Ahora con las aceleraciones
-a = np.zeros(v_un_frame*n_frames)
+#a = np.zeros(v_un_frame*n_frames)
+num_bins = 1000
+binis = np.linspace(-7000, 7000, num_bins)
+Ac = np.zeros(num_bins-1)
 
-with h5py.File(ftp_hdf, 'r') as f:
-    gdp = f.get('dif_phase')
-    h_dp = gdp['dp']
-    for i, j in zip(range(1,len(h_dp)), tqdm(range(1,len(h_dp)))):
-        dp1 = h_dp[i][10:-10,10:-10]
-        dp0 = h_dp[i-1][10:-10,10:-10]
+#with h5py.File('18-12-2021_ftp.hdf5', 'r') as f:
+with h5py.File('alturas.hdf5', 'r') as f:
+    gdp = f.get('hs')
+    h_dp = gdp['alts']
+    dp0 = h_dp[0][10:-10,10:-10]
+    dp1 = h_dp[1][10:-10,10:-10]
+    for i, j in zip(range(1,len(h_dp)-1), tqdm(range(1,len(h_dp)-1))):
+#        dp1 = h_dp[i][10:-10,10:-10]
+#        dp0 = h_dp[i-1][10:-10,10:-10]
         dp2 = h_dp[i+1][10:-10,10:-10]
-        aceleracion = (250**2)*(dp2-2*dp0+dp1)
+        aceleracion = (250**2)*(dp2-2*dp1+dp0)
         aceleracion = aceleracion.flatten()
         his, bins = np.histogram(aceleracion, bins=binis)
-        del(aceleracion, dp1, dp0, dp2)
-
+        Ac += his
+        dp0 = dp1
+        dp1 = dp2
+#        print( np.mean(aceleracion), np.max(aceleracion), np.min(aceleracion))
+#        del(aceleracion, dp1, dp0, dp2)
+#%%
 #Hago el histograma de aceleración
-norm = np.sum(H)
+norm = np.sum(Ac)
 plt.figure()
-plt.bar(binis[:-1],H/norm,width=binis[1]-binis[0],edgecolor='black')
+plt.bar(binis[:-1],Ac/norm,width=binis[1]-binis[0],edgecolor='black')
 plt.grid()
 plt.title('Histograma de Aceleración')
 plt.show()
+#%%
