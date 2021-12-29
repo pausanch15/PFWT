d1f7fd6c3b40b8c7ac12a587448f9ba705efbb14
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-12-29 18:35:38 -0300

    Update histogramas_vya.py

--- histogramas_vya.py
+++ histogramas_vya.py
@@ -9,7 +9,11 @@ from time import time
 import h5py 
 from tqdm import tqdm
 plt.ion()
-
+#%%
+def normal(x,mu,sig):
+    A = 1 / (sig * np.sqrt(2*np.pi))
+    ex = (x - mu)**2 / (2 * sig**2)
+    return A * np.exp(-ex)
 #%%
 #Traigo los hdf5 con las alturas y hago el histograma de las alturas
 num_bins = 1000
@@ -25,17 +29,9 @@ with h5py.File('alturas.hdf5', 'r') as f:
         his, bins = np.histogram(hs, bins=binis)
         Al += his
 #%%
-norm = np.sum(Al)
-plt.figure()
-plt.bar(binis[:-1], height=Al/norm, width=binis[1]-binis[0], edgecolor='black')
-plt.grid()
-plt.title('Histograma de Alturas')
-plt.show()
-
-#%%
 #Ahora el de las velocidades
 num_bins = 1000
-binis = np.linspace(-40, 40, num_bins)
+binis = np.linspace(-70, 70, num_bins)
 Ve = np.zeros(num_bins-1)
 
 #with h5py.File('18-12-2021_ftp.hdf5', 'r') as f:
@@ -50,14 +46,6 @@ with h5py.File('alturas.hdf5', 'r') as f:
         his, bins = np.histogram(vel,bins=binis)
         Ve += his
         dp0 = dp1
-#%%
-norm = np.sum(Ve)
-plt.figure()
-plt.bar(binis[:-1],Ve/norm,width=binis[1]-binis[0],edgecolor='black')
-plt.show()
-plt.grid()
-plt.title('Histograma de Velocidades')
-plt.show()
 #%%
 #Ahora con las aceleraciones
 #a = np.zeros(v_un_frame*n_frames)
@@ -84,11 +72,70 @@ with h5py.File('alturas.hdf5', 'r') as f:
 #        print( np.mean(aceleracion), np.max(aceleracion), np.min(aceleracion))
 #        del(aceleracion, dp1, dp0, dp2)
 #%%
+#==============================================================================
+# Hago hist
+#==============================================================================
+norm = np.sum(Al)
+
+binis = np.linspace(-5, 5, num_bins)
+bic = (binis[1:] + binis[:-1]) / 2
+mu = np.sum(bic * Al / norm)
+sig = np.sqrt( np.sum( (bic-mu)**2 * Al / norm) )
+print(mu,sig)
+nor = normal(bic,mu,sig)
+norma = np.sum(nor)
+
+binis = np.linspace(-5, 5, num_bins)
+plt.figure()
+plt.bar(binis[:-1], height=Al/norm, width=binis[1]-binis[0], edgecolor='black')
+plt.plot(bic,nor/norma,'r-')
+plt.yscale('log')
+plt.ylim(1e-8,0.03)
+plt.xlim(-2,2)
+plt.grid()
+plt.title('Histograma de Alturas')
+plt.show()
+
+#%%
+norm = np.sum(Ve)
+binis = np.linspace(-70, 70, num_bins)
+
+bic = (binis[1:] + binis[:-1]) / 2
+mu = np.sum(bic * Ve / norm)
+sig = np.sqrt( np.sum( (bic-mu)**2 * Ve / norm) )
+print(mu,sig)
+nor = normal(bic,mu,sig)
+norma = np.sum(nor)
+
+plt.figure()
+plt.bar(binis[:-1],Ve/norm,width=binis[1]-binis[0],edgecolor='black')
+plt.plot(bic,nor/norma,'r-')
+plt.yscale('log')
+plt.ylim(1e-10,0.07)
+plt.xlim(-60,60)
+plt.grid()
+plt.title('Histograma de Velocidades')
+plt.show()
+#%%
 #Hago el histograma de aceleración
 norm = np.sum(Ac)
+binis = np.linspace(-7000, 7000, num_bins)
+
+bic = (binis[1:] + binis[:-1]) / 2
+mu = np.sum(bic * Ac / norm)
+sig = np.sqrt( np.sum( (bic-mu)**2 * Ac / norm) )
+print(mu,sig)
+nor = normal(bic,mu,sig)
+norma = np.sum(nor)
+
+
 plt.figure()
 plt.bar(binis[:-1],Ac/norm,width=binis[1]-binis[0],edgecolor='black')
+plt.plot(bic,nor/norma,'r-')
 plt.grid()
+plt.yscale('log')
+plt.ylim(1e-7,0.04)
+#plt.xlim(-60,60)
 plt.title('Histograma de Aceleración')
 plt.show()
 #%%
