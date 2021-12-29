a99cca1345b5c26c03ba8e3d094de34c89962c47
Tomas-Ferreyra <tomasferreyra21@yahoo.com.ar>
2021-12-29 18:35:17 -0300

    Update ener_ftp_otro.py

--- ener_ftp_otro.py
+++ ener_ftp_otro.py
@@ -4,17 +4,18 @@ import numpy as np
 from time import time
 #%%
 t1 = time()
-na = 10
+na = 100
 ali = np.zeros((1004,1004,na)) + 1j *  np.zeros((1004,1004,na))
 with h5py.File('alturas.hdf5', 'r') as f:
     gdp = f.get('hs')
     alturas = gdp['alts']
     for i, ñ in zip(range(na), tqdm(range(na))):
         loll = alturas[i][10:-10,10:-10]
+#        ali[:,:,i] = loll
         ali[:,:,i] = np.fft.fft2(loll)
-with h5py.File('alturas.hdf5', 'w') as f:
+with h5py.File('alturasss.hdf5', 'w') as f:
     fourier_im = f.create_group('fi')
-    transs = fourier_im.create_dataset('trans', shape=(1004,1004, 3072), dtype='complex')
+    transs = fourier_im.create_dataset('trans', shape=(1004,1004, 500), dtype='complex')
     transs[:,:,:na] = ali
 t2 = time()
 t2 - t1
